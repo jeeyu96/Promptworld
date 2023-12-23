70800b675d2558933364da231571cd96c16b4b5f
jeeyu96 <54443672+jeeyu96@users.noreply.github.com>
2023-12-23 08:52:02 +0100

    add create button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,10 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+
+  // mock the state the user is logged in or not
+  const isUserLoggedIn = true;
+
   return (
     // navigation structure
     <nav className="flex-between w-full mb-16 pt-3">
@@ -19,6 +23,20 @@ const Nav = () => {
         />
         <p className="logo_text"> Promptworld </p>
       </Link>
+
+      {/* Mobile Navigation */}
+      <div className ="sm:flex hidden">
+        {isUserLoggedIn? (
+          <div className ="flex gap-3 md:gap-5"> 
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ): (
+          <>
+          </>
+        )}
+      </div>
     </nav>
   )
 }
